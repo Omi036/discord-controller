8e8e4d387226500d12e3225cfbbecf377810710e
Omi036 <alvarogm273@gmail.com>
2023-02-19 15:19:48 +0100

    Added "Add Embed" Beta functionability

--- web/src/components/pages/Messages/AddEmbedModal.jsx
+++ web/src/components/pages/Messages/AddEmbedModal.jsx
@@ -1,20 +1,56 @@
-import { Box, Modal, Textarea , TextInput, ColorInput, Divider} from "@mantine/core"
+import { Box, Modal, Textarea , TextInput, ColorInput, Divider, Text, Button, Image} from "@mantine/core"
+import { useState, useRef } from "react"
 
 export const AddEmbedModal = ({opened, setOpened}) => {
+    const [color, setColor] = useState("#fff")
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [author, setAuthor] = useState("")
+    const [image, setImage] = useState("")
+    const [thumbnail, setThumbnail] = useState("")
+    const [footer, setFooter] = useState("")
+
     return (
-        <Modal opened={opened} onClose={()=>setOpened(false)} size={900} title="Create Embed">
-            <Box style={{display: 'flex', flexDirection: 'row'}}>
+        <Modal opened={opened} onClose={()=>setOpened(false)} size={1100}  title="Create Embed">
+            <Box style={{display: 'flex', flexDirection: 'row', height:"37rem"}}>
                 <Box style={{width:"30%"}}>
-                    <TextInput label="Title" required />
-                    <Textarea label="Description" required />
-                    <ColorInput label="Embed Color"/>
-                    <TextInput label="Author" />
-                    <TextInput label="Footer" />
-                    <TextInput label="Thumbnail URL" />
-                    <TextInput label="Image URL" />
+                    <TextInput label="Title" required value={title} onChange={(event) => setTitle(event.currentTarget.value)}/>
+                    <Textarea label="Description" required value={description} onChange={(event) => setDescription(event.currentTarget.value)}/>
+                    <ColorInput label="Embed Color" maxLength={7} value={color} onChange={setColor}/>
+                    <TextInput label="Author" value={author} onChange={(event) => setAuthor(event.currentTarget.value)}/>
+                    <TextInput label="Footer" value={footer} onChange={(event) => setFooter(event.currentTarget.value)}/>
+                    <TextInput label="Thumbnail URL" value={thumbnail} onChange={(event) => setThumbnail(event.currentTarget.value)}/>
+                    <TextInput label="Image URL" value={image} onChange={(event) => setImage(event.currentTarget.value)}/>
+                    <Button color="indigo" fullWidth style={{marginTop:10}}>Add Embed</Button>
                 </Box>
                 <Divider orientation="vertical" variant="dashed" style={{marginLeft: 10, marginRight:10}}/>
-                <Box>
+                <Box style={{width:"69%", height:"100%"}}>
+                    <Box sx={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems: "center", height:"100%"}}>
+
+                        {/* Embed Preview */}
+                        <Box style={{width:"80%", borderRadius:5,overflow: "hidden",minHeight:"10%", display:"flex", flexDirection:"row"}}>
+                            {/* Color */}
+                            <Box style={{backgroundColor:color,height:"100%", width:"1%"}}></Box>
+                            {/* Content */}
+                            <Box style={{backgroundColor:"#2f3136", width:"99%", height:"100%", padding:10, display:"flex", flexDirection:"row"}}>
+                                <Box style={{maxWidth:thumbnail ? "70%" : "100%"}}>
+                                    <Text weight="bold" size={14} style={{marginBottom:7}}>{author}</Text>
+                                    <Text weight="bold" size={18} style={{marginBottom:7}}>{title}</Text>
+                                    <Text>{description}</Text>
+
+                                    {image && <Image radius={10} style={{marginTop:10, marginBottom:10}} src={image} />}
+
+                                    {footer && <Text size={11} style={{marginTop:image ? 0 : 5}} weight="bold">{footer}</Text>}
+                                </Box>
+                                {/* Thumbnail */}
+                                <Box style={{width:"30%", marginLeft:5}}>
+                                    {thumbnail && <Image radius={10} style={{width:"100%"}} src={thumbnail} />}
+                                </Box>
+                            </Box>
+                        </Box>
+                        
+                    </Box>
+
 
                 </Box>
             </Box>
--- web/src/components/pages/Messages/Embed.jsx
+++ web/src/components/pages/Messages/Embed.jsx
@@ -2,37 +2,39 @@ import { Box, Text, Image } from "@mantine/core"
 
 export const Embed = ({author, color, description, footer, image, thumbnail, timestamp, title, fields}) => {
     return (
-        <Box style={{backgroundColor:color || "#242526", width:"90%", borderRadius:10, overflow:"hidden", boxSizing:"border-box", paddingLeft:5, maxWidth:"60%"}}>
+        <Box style={{backgroundColor:color || "#242526", borderRadius:10, overflow:"hidden", boxSizing:"border-box", paddingLeft:5, maxWidth:"55%"}}>
 
             {/* True Embed */}
-            <Box style={{backgroundColor:"#2f3136", boxSizing:"border-box", paddingLeft:10, paddingRight:10, paddingBottom:10, paddingTop:10}}>
+            <Box style={{backgroundColor:"#2f3136", boxSizing:"border-box", padding:10, width:"100%", display:"flex", flexDirection:"row"}}>
 
-                {/* Upper Section, title, description and thumbnail */}
-                <Box style={{display:"flex"}}>
-                    <Box>
-                        <Text weight="bold" size={14} style={{marginBottom:7}}>{author}</Text>
-                        <Text weight="bold" size={18} style={{marginBottom:7}}>{title}</Text>
-                        <Text style={{marginBottom:7}}>{description}</Text>
+                <Box style={{boxSizing:"border-box", maxWidth:"70%"}}>
+                    {/* Upper Section, title, description and thumbnail */}
+                    <Text weight="bold" size={14} style={{marginBottom:7}}>{author}</Text>
+                    <Text weight="bold" size={18} style={{marginBottom:7}}>{title}</Text>
+                    <Text style={{marginBottom:30}}>{description}</Text>
+
+                    {/* Fields */}
+                    {fields.map(field => (
+                        <Box key={`${field.name}${field.value}`} style={{marginBottom:14}}>
+                            <Text weight="bold">{field.name.replaceAll("*","").replaceAll("_","")}</Text>
+                            <Text>{field.value.replaceAll("*","").replaceAll("_","")}</Text>
+                        </Box>
+                    ))}
+
+                    {/* Image */}
+                    {image && <Image radius={10} style={{marginTop:10, marginBottom:10}} src={image} />}
+
+                    {/* Footer */}
+                    <Box style={{display: 'flex', marginTop:0}}>
+                        {footer && <Text size={11} style={{marginTop:0,}} weight="bold">{footer}</Text>}
+                        {footer && timestamp && (<Text size={11} style={{marginLeft:5, marginRight:5}} weight="bold">•</Text>)}
+                        {timestamp && <Text size={11} weight="bold">{new Date(timestamp).toDateString()}</Text>}
                     </Box>
-                    {thumbnail && <Image radius={10} style={{marginTop:10, marginLeft:10, marginBottom:10}} src={thumbnail} />}
                 </Box>
 
-                {/* Fields */}
-                {fields.map(field => (
-                <Box key={`${field.name}${field.value}`} style={{marginBottom:14}}>
-                    <Text weight="bold">{field.name}</Text>
-                    <Text>{field.value}</Text>
-                </Box>
-                ))}
-
-                {/* Image */}
-                {image && <Image radius={10} style={{marginTop:10, marginBottom:10}} src={image} />}
-
-                {/* Footer */}
-                <Box style={{display: 'flex'}}>
-                    <Text size={11} weight="bold">{footer}</Text>
-                    {footer && timestamp && (<Text size={11} style={{marginLeft:5, marginRight:5}}weight="bold">•</Text>)}
-                    <Text size={11} weight="bold">{new Date(timestamp).toDateString()}</Text>
+                {/* Thumbnail Image */}
+                <Box style={{width:"fit-content", minWidth:"30%"}}>
+                    {thumbnail && <Image radius={10} style={{width:"100%"}} src={thumbnail} />}
                 </Box>
 
             </Box>

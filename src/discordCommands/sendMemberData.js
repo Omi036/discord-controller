0ca49b76ae6d63b60663991f5337f9adc7565342
Omi036 <alvarogm273@gmail.com>
2023-02-26 20:17:16 +0100

    Fixed bug that didn't show members with 0 roles

--- src/discordCommands/sendMemberData.js
+++ src/discordCommands/sendMemberData.js
@@ -4,18 +4,21 @@ exports.sendMemberData = (client, connection, svId, memberId, intents, GatewayIn
         member.user.fetch().then(user => {
             var status = "offline";
             var statusDevice;
+            var filtered_roles = {}
 
             if(intents.includes(GatewayIntentBits.GuildPresences) && member.presence ) {
                 status = member.presence.status
                 statusDevice = Object.keys(member.presence.clientStatus).join(" ")
             }
 
-            var filtered_roles = {}
             var roles = member.roles.cache
             roles.sort((a, b) => {return b.position - a.position})
-            for(const role of roles) {
-                if(role[1].name === "@everyone") continue
-                filtered_roles[role[1].name] = {color:role[1].hexColor,id:role[1].id
+            
+            if(roles.size > 1) {
+                for(const role of roles) {
+                    if(role[1].name === "@everyone") continue                    
+                    filtered_roles[role[1].name] = {color:role[1].hexColor,id:role[1].id}
+                }
             }
 
 
@@ -43,6 +46,6 @@ exports.sendMemberData = (client, connection, svId, memberId, intents, GatewayIn
                     permissions: member.permissions.toArray()
                 }
             }))
-        }})
+        })
     })
 }
